5f06fb11bd30d1935b169f10a9fdb9c625316cfd
DaviCM <davimaciel2008@gmail.com>
2026-03-11 11:47:23 -0300

    Alteração no gerenciador de sessão

--- src/api/goals_routes.py
+++ src/api/goals_routes.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException # importar do fast api o roteador
 from src.models.goal_model import Goal
-from src.api.dependencies import pegar_sessao
+from src.database.session import get_session
 from src.schemas.goals_schemas import GoalsSchema, UpdateGoalsSchema
 
 
 goals_router = APIRouter(prefix= "/goals", tags= ["metas"])
 
 @goals_router.post("/criar_metas")
-async def create_goals(goals_schema: GoalsSchema, session = Depends(pegar_sessao)):
+async def create_goals(goals_schema: GoalsSchema, session = Depends(get_session)):
      new_goal = Goal(
     starting_date=goals_schema.starting_date,
     ending_date=goals_schema.ending_date,
@@ -20,7 +20,7 @@ async def create_goals(goals_schema: GoalsSchema, session = Depends(pegar_sessao
 
 
 @goals_router.get("/listar_metas")
-async def list_goals(session = Depends(pegar_sessao)):
+async def list_goals(session = Depends(get_session)):
     goals_list = session.query(GoalsSchema).all()
     if not goals_list:
         raise HTTPException(status_code=404, detail="Metas não encontradas")
@@ -28,7 +28,7 @@ async def list_goals(session = Depends(pegar_sessao)):
         return{"mensagem": goals_list }
 
 @goals_router.put("/editar_meta_de_consumo")
-async def change_consumption(goals_schema: UpdateGoalsSchema , session = Depends(pegar_sessao)):
+async def change_consumption(goals_schema: UpdateGoalsSchema , session = Depends(get_session)):
     goal = session.query(Goal).filter(Goal.id == goals_schema.id).first()
     if not goal:
         raise HTTPException(status_code=404, detail = "Registros de consumo não encontrado")
@@ -44,7 +44,7 @@ async def change_consumption(goals_schema: UpdateGoalsSchema , session = Depends
     return{"mensagem" : "Metas de consumo alterados com sucesso"}
 
 @goals_router.delete("/deletar_meta/{id}")
-async def delete_goals(id: int, session = Depends(pegar_sessao)):
+async def delete_goals(id: int, session = Depends(get_session)):
     goals = session.query(Goal).filter(Goal. id == id).first()
     if not goals:
         raise HTTPException(status_code=404, detail="Metas não encontradas")
